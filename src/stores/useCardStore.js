16e9665d874e8835df703ec3f07f332bd3056e07
PabloIxcamparij <j.pablo.sorto@gmail.com>
2024-09-08 20:57:06 -0600

    feat: add Notification modal and enhance card management
    - Added `Notification` modal; installed dependencies: `@headlessui/react` and `@heroicons/react`.
    - Renamed `cardsData` to `handCards` in `PlaceGame` component and `useCardStore` for better clarity.
    - Implemented functionality for discard and play buttons in `PlaceGame` component.
    - Integrated `Notification` modal into `PlaceGame` component.
    - Enhanced `useCardStore` with `get` functionality alongside `set`.
    - Added `discardCards` vector to serve as the deck; populated after initial card selection with `selectRandomCards`.
    - Implemented `handleDiscardCards` to remove selected cards and replace them with new ones.
    - Added `handlePlayCards` to identify the current hand being played.
    - Created notification system: `showNotification` to display notifications and `hideNotification` to close them.

--- src/stores/useCardStore.js
+++ src/stores/useCardStore.js
@@ -4,22 +4,29 @@ import { Cards } from "../Data/Card.json";
 
 export const useCardStore = create(
   devtools(
-    (set) => ({
+    (set, get) => ({
       cardsData: Cards,
-      selectedCards: [], // Nuevo estado para los IDs de las cartas seleccionadas
+      handCards: [],
+      selectedCards: [],
+      discardCards: [],
+
+      // discardCards apartir que se hace la selccion actua
+      // como el mazo de cartas,
+      // y sera a este al que se le van quitando cartas
 
       // Acción para agregar o eliminar una carta seleccionada
-      toggleCardId: (cardId) => set((state) => {
-        const isSelected = state.selectedCards.includes(cardId);
-        
-        return {
-          selectedCards: isSelected
-            ? state.selectedCards.filter((id) => id !== cardId)
-            : state.selectedCards.length < 5
-            ? [...state.selectedCards, cardId]
-            : state.selectedCards,
-        };
-      }),
+      toggleCardId: (cardId) =>
+        set((state) => {
+          const isSelected = state.selectedCards.includes(cardId);
+
+          return {
+            selectedCards: isSelected
+              ? state.selectedCards.filter((id) => id !== cardId)
+              : state.selectedCards.length < 5
+              ? [...state.selectedCards, cardId]
+              : state.selectedCards,
+          };
+        }),
 
       // Función para seleccionar 9 cartas aleatorias
       selectRandomCards: () => {
@@ -30,15 +37,156 @@ export const useCardStore = create(
 
         const selectedFromFirstHalf = firstHalf
           .sort(() => 0.5 - Math.random())
-          .slice(0, 4);
+          .slice(0, 5);
         const selectedFromSecondHalf = secondHalf
           .sort(() => 0.5 - Math.random())
           .slice(0, 4);
 
         const selected = [...selectedFromFirstHalf, ...selectedFromSecondHalf];
-        return set({ cardsData: selected });
+
+        set({
+          handCards: selected,
+
+          // Actualiza discardCards excluyendo las cartas en handCards
+          discardCards: Cards.filter((card) => !selected.includes(card)),
+        });
+      },
+
+      // Función para descartar y obtener nuevas cartas aleatorias
+      handleDiscardCards: () => {
+        const { selectedCards, handCards, discardCards, showNotification} = get();
+
+        if (selectedCards.length === 0) {
+          showNotification({
+            text: "No se ha seleccionado ninguna carta",
+            error: true,
+          });
+          return;
+        }
+
+        const remainingHandCards = handCards.filter(
+          (card) => !selectedCards.includes(card)
+        );
+
+        console.log(remainingHandCards);
+
+        // Seleccionar nuevas cartas aleatorias de discardCards
+        const shuffledDiscardCards = [...discardCards].sort(
+          () => 0.5 - Math.random()
+        );
+
+        const newCards = shuffledDiscardCards.slice(0, selectedCards.length);
+
+        // Excluir las cartas seleccionadas
+        const updatedDiscardCards = discardCards.filter(
+          (card) => !newCards.includes(card)
+        );
+
+        // Actualizar el estado con las nuevas cartas
+        set({
+          handCards: [...remainingHandCards, ...newCards],
+          discardCards: updatedDiscardCards,
+          selectedCards: [],
+        });
+      },
+
+      // Función para comprobar que valor tiene la mano juagada
+      handlePlayCards: () => {
+        const { selectedCards, handleDiscardCards, showNotification } = get();
+
+        if (selectedCards.length === 0) {
+          showNotification({
+            text: "No se ha seleccionado ninguna carta",
+            error: true,
+          });
+          return;
+        }
+
+        // Ordenar cartas por valor
+        const sortedCards = [...selectedCards].sort(
+          (a, b) => a.valor - b.valor
+        );
+
+        // Obtener solo los valores de las cartas
+        const values = sortedCards.map((card) => card.valor);
+
+        const counts = {};
+        values.forEach((value) => {
+          counts[value] = (counts[value] || 0) + 1;
+        });
+
+        const countValues = Object.values(counts);
+
+        // Verificar las combinaciones
+
+        // Si hay una pareja y solo una combinación
+        if (countValues.includes(2) && countValues.length === 1) {
+          showNotification({
+            text: "Pareja (One Pair): 20",
+            error: false,
+          });
+        }
+
+        // Si hay dos parejas
+        else if (countValues.filter((count) => count === 2).length === 2) {
+          showNotification({
+            text: "Doble Pareja (Two Pair): 30",
+            error: false,
+          });
+        }
+
+        // Ninguna combinación
+        else if (
+          countValues.includes(1) &&
+          countValues.length === selectedCards.length
+        ) {
+          showNotification({
+            text: "Carta Alta (High Card): 10",
+            error: false,
+          });
+        }
+
+        // Mensaje de error
+        else {
+          showNotification({
+            text: "No hay una combinación válida",
+            error: true,
+          });
+        }
+
+        // Se llama al descarte de cartas para eliminar las cartas jugadas
+        handleDiscardCards();
+      },
+
+      // Funciones para mostrar la notificacion
+      notificacion: {
+        text: "",
+        error: false,
+        show: false,
+      },
+
+      showNotification: (payload) => {
+        set({
+          notificacion: {
+            text: payload.text,
+            error: payload.error,
+            show: true,
+          },
+        });
+
+        setTimeout(() => {
+          get().hideNotification();
+        }, 3000);
+      },
+
+      hideNotification: () => {
+        set({
+          notificacion: {
+            show: false,
+          },
+        });
       },
     }),
-    { name: "CardStore" } // Opcional, le da un nombre al store en DevTools
+    { name: "CardStore" }
   )
 );
--- src/views/PlaceGame.jsx
+++ src/views/PlaceGame.jsx
@@ -1,27 +1,39 @@
 import React, { useEffect } from "react";
 import Jokers from "../components/Jokers";
+import Notification from "../components/Notification";
 import { Button } from "@nextui-org/react";
 import { useCardStore } from "../stores/useCardStore";
 
 export default function PlaceGame() {
-
-  const { cardsData, selectedCards, toggleCardId, selectRandomCards } =
-    useCardStore((state) => ({
-      cardsData: state.cardsData,
-      selectedCards: state.selectedCards,
-      toggleCardId: state.toggleCardId,
-      selectRandomCards: state.selectRandomCards,
-    }));
+  const {
+    handCards,
+    selectedCards,
+    toggleCardId,
+    selectRandomCards,
+    handleDiscardCards,
+    handlePlayCards,
+  } = useCardStore((state) => ({
+    handCards: state.handCards,
+    selectedCards: state.selectedCards,
+    toggleCardId: state.toggleCardId,
+    selectRandomCards: state.selectRandomCards,
+    handleDiscardCards: state.handleDiscardCards,
+    discardCards: state.discardCards,
+    handlePlayCards: state.handlePlayCards,
+  }));
 
   useEffect(() => {
     selectRandomCards();
   }, [selectRandomCards]);
 
-  console.log(selectedCards)
+  console.log(selectedCards);
+  console.log(handCards);
 
   return (
     <div className="flex flex-col justify-between items-center min-h-screen bg-[#F2E8CF] p-3 overflow-auto text-white">
       
+      <Notification />
+
       <div className="flex justify-between w-full items-center p-5">
         <Jokers />
         <h1 className="text-danger font-bold text-xl">
@@ -30,11 +42,11 @@ export default function PlaceGame() {
       </div>
 
       <div className="flex flex-wrap justify-around items-center w-full gap-6">
-        
         <Button
           className="rounded-full w-2/5 font-bold text-xl"
           color="danger"
           variant="ghost"
+          onClick={() => handleDiscardCards()}
         >
           Descartar
         </Button>
@@ -42,13 +54,14 @@ export default function PlaceGame() {
           className="rounded-full w-2/5 font-bold text-xl"
           color="success"
           variant="ghost"
+          onClick={() => handlePlayCards()}
         >
           Jugar
         </Button>
 
         {/* Cartas seleccionadas */}
         <div className="h-2/4 w-full bg-custom-green-dark rounded-xl flex flex-wrap justify-around items-center gap-4 p-4 mb-5">
-          {cardsData.map((card, index) => (
+          {handCards.map((card) => (
             <div
               key={card.id}
               onClick={() => toggleCardId(card)}
@@ -63,7 +76,6 @@ export default function PlaceGame() {
             </div>
           ))}
         </div>
-
       </div>
     </div>
   );
