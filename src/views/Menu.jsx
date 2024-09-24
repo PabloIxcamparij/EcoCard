6751ac0d13d92819841f37d9b750373d05fb2799
PabloIxcamparij <j.pablo.sorto@gmail.com>
2024-09-23 19:50:04 -0600

    chore: changes in routes in ButtonMenu

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -3,8 +3,8 @@ import { ChartBarIcon, DocumentTextIcon, PlayCircleIcon  } from "@heroicons/reac
 
 export default function App() {
   return (
-    <div className="flex w-full h-screen flex-col justify-center items-center">
-      <div className="w-5/6 bg-custom-green flex justify-center items-center rounded-3xl p-5 border-b-8 border-r-8 border-custom-green-dark text-white">
+    <div className="flex w-full flex-col justify-center items-center">
+      <div className="w-5/6 bg-custom-green p-5 mt-10 rounded-3xl border-b-8 border-r-8 border-custom-green-dark text-white">
         <h1 className="text-4xl font-bold text-wrap text-center">Eco Card</h1>
       </div>
 
